fc637471bda7ea3fd17713708d42c6dd48f0b12d
Kruzenger <kruzenger@gmail.com>
2025-06-24 14:29:02 +0300

    romoved legacy code

--- trash_or_old/Consumer/DataTransitionManager.py
+++
@@ -1,8 +0,0 @@
-from KafkaManager import KafkaManager
-
-class DataTransitionManager:
-    def __init__(self):
-        self.kafka_manager = KafkaManager()
-
-
-DATA_TRN_MNG = DataTransitionManager()
